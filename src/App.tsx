89dfd9d42a808f8b06ca3d8c86683d5509274ac3
zippo <zippomen111@gmail.com>
2023-04-17 18:01:09 +0100

    lazy loading for Cart, FullItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import React, { Suspense } from 'react';
 import './scss/app.scss';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
-import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
 import FullItems from './pages/FullItems';
+const Cart = React.lazy(() => import('./components/Cart'));
 function App() {
 
   return (
@@ -12,8 +13,12 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/item/:id" element={<FullItems />} />
+        <Route path="/cart" element={<Suspense fallback={<div>loading...</div>}>
+          <Cart />
+        </Suspense>} />
+        <Route path="/item/:id" element={<Suspense fallback={<div>loading...</div>}>
+          <FullItems />
+        </Suspense>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
 
